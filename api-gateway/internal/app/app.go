3096cc9fb57219184d647384d04427a4f34b1ae2
Ddarli <ivanlatyshev024@gmail.com>
2025-03-03 18:43:07 +0300

    auth server

--- api-gateway/internal/app/app.go
+++ api-gateway/internal/app/app.go
@@ -1,1 +1,16 @@
 package app
+
+import (
+	"github.com/Ddarli/svc/gateway/conf"
+	"github.com/Ddarli/svc/gateway/internal/handler"
+	"github.com/gofiber/fiber/v2"
+	"log"
+)
+
+func Run(cfg *conf.Conf) {
+	app := fiber.New()
+	
+	handler.SetupRoutes(app, cfg)
+
+	log.Fatal(app.Listen(":" + cfg.Server.Port))
+}
--- auth/internal/app/app.go
+++ auth/internal/app/app.go
@@ -1,1 +1,13 @@
 package app
+
+import (
+	"auth/conf"
+	"github.com/gofiber/fiber/v2"
+	"log"
+)
+
+func Run(configuration *conf.Configuration) {
+	app := fiber.New()
+
+	log.Fatal(app.Listen(":" + configuration.Server.Port))
+}
